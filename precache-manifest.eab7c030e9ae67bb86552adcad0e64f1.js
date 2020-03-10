558ba6f99f528cb826bb5ea22da045169e5112c9
Jefferson-Afanou <49483840+Jefferson-Afanou@users.noreply.github.com>
2020-03-10 03:28:04 +0100

    Updates

---
+++ precache-manifest.eab7c030e9ae67bb86552adcad0e64f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d2ded7bb18baf566c0f05922f496912",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4226820205443501b8e3",
+    "url": "/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "36628daaae93ad211a34",
+    "url": "/static/js/2.c2e4b901.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.c2e4b901.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4226820205443501b8e3",
+    "url": "/static/js/main.ed2a9e8a.chunk.js"
+  },
+  {
+    "revision": "4567734cd469a9475f6c",
+    "url": "/static/js/runtime-main.736723a6.js"
+  }
+]);
