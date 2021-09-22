f10d2256522f2a583803407121ed985aee34739e
Jefferon AFANOU <jefferson.afanou@gmail.com>
2021-09-22 02:07:55 +0200

    Quick Update 2021

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -1,37 +1,66 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Introduction extends Component {
   render() {
     return (
       <div>
-        <section id="colorlib-hero" className="js-fullheight" data-section="home">
+        <section
+          id="colorlib-hero"
+          className="js-fullheight"
+          data-section="home"
+        >
           <div className="flexslider js-fullheight">
             <ul className="slides">
-              <li style={{backgroundImage: 'url(images/portfolio_home.jpg)'}}>
+              <li style={{ backgroundImage: "url(images/portfolio_home.jpg)" }}>
                 <div className="overlay" />
                 <div className="container-fluid">
                   <div className="row">
                     <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                       <div className="slider-text-inner js-fullheight">
-                        <div className="desc" style={{marginTop:"30%"}}>
-                          <h1 style={{marginBottom:0}}>Bonjour, <br />Je m'appelle Jefferson</h1>
+                        <div className="desc" style={{ marginTop: "30%" }}>
+                          <h1 style={{ marginBottom: 0 }}>
+                            Bonjour, <br />
+                            Je m'appelle Jefferson
+                          </h1>
                           <p>(Et la feuille blanche à côté, c'est moi.)</p>
-                          <p><a className="btn btn-primary btn-learn" href="https://drive.google.com/file/d/1W4gNq3qDkiRytN8KlDYgj5exPNK8ck6L/view?usp=sharing" target="_blank" rel="noopener noreferrer" style={{marginTop: "5%"}}>Voir mon CV  <i className="icon-download4" /></a></p>
+                          <p>
+                            <a
+                              className="btn btn-primary btn-learn"
+                              target="_blank"
+                              href="https://drive.google.com/file/d/1Tv30VwlXeb6TDpIzxfwrJQmuPIYoaXJ_/view?usp=sharing"
+                              target="_blank"
+                              rel="noopener noreferrer"
+                              style={{ marginTop: "5%" }}
+                            >
+                              Voir mon CV <i className="icon-download4" />
+                            </a>
+                          </p>
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </li>
-              <li style={{backgroundImage: 'url(images/portfolio_home.jpg)'}}>
+              <li style={{ backgroundImage: "url(images/portfolio_home.jpg)" }}>
                 <div className="overlay" />
                 <div className="container-fluid">
                   <div className="row">
                     <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                       <div className="slider-text-inner">
-                        <div className="desc" style={{marginTop:"30%"}}>
+                        <div className="desc" style={{ marginTop: "30%" }}>
                           <h1>Vous cherchez du code ?</h1>
-                          <p> <a className="btn btn-primary btn-learn" href="https://github.com/afanou-jefferson" target="_blank" rel="noopener noreferrer">Voir mon Github <i className="icon-github"/> </a> </p>
+                          <p>
+                            {" "}
+                            <a
+                              className="btn btn-primary btn-learn"
+                              target="_blank"
+                              href="https://github.com/afanou-jefferson"
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              Voir mon Github <i className="icon-github" />{" "}
+                            </a>{" "}
+                          </p>
                         </div>
                       </div>
                     </div>
@@ -42,6 +71,6 @@ export default class Introduction extends Component {
           </div>
         </section>
       </div>
-    )
+    );
   }
 }
--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,46 +1,104 @@
-import React, { Component } from 'react'
-import {FaCode} from 'react-icons/fa'
+import React, { Component } from "react";
+import { FaCode } from "react-icons/fa";
 
 export default class Sidebar extends Component {
   render() {
     return (
       <div>
         <div>
-          <nav href="#navbar" className="js-colorlib-nav-toggle colorlib-nav-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><i /></nav>
-          <aside id="colorlib-aside" className="border js-fullheight" style={{overflow: "hidden"}}>
+          <nav
+            href="#navbar"
+            className="js-colorlib-nav-toggle colorlib-nav-toggle"
+            data-toggle="collapse"
+            data-target="#navbar"
+            aria-expanded="false"
+            aria-controls="navbar"
+          >
+            <i />
+          </nav>
+          <aside
+            id="colorlib-aside"
+            className="border js-fullheight"
+            style={{ overflow: "hidden" }}
+          >
             <div className="text-center">
-              <div className="author-img" style={{backgroundImage: 'url(images/photo_JA.jpg)'}} />
-              <h1 id="colorlib-logo"><a href="index.html" style={{marginBottom: "5%"}}>Jefferson Afanou</a></h1>
-              <span className="email"><i className="icon-mail" ></i>jefferson.afanou@mail.com</span>
+              <div
+                className="author-img"
+                style={{ backgroundImage: "url(images/avatar_JA.png)" }}
+              />
+              <h1 id="colorlib-logo">
+                <a href="index.html" style={{ marginBottom: "5%" }}>
+                  Jefferson Afanou
+                </a>
+              </h1>
+              <span className="email">
+                <i className="icon-mail"></i>jefferson.afanou@mail.com
+              </span>
             </div>
             <nav id="colorlib-main-menu" role="navigation" className="navbar">
-              <div id="navbar" className="collapse" style={{marginTop: "5%"}}>
+              <div id="navbar" className="collapse" style={{ marginTop: "5%" }}>
                 <ul>
-                  <li className="active"><a href="#home" data-nav-section="home">Présensation</a></li>
+                  <li className="active">
+                    <a href="#home" data-nav-section="home">
+                      Présensation
+                    </a>
+                  </li>
                   {/*<li><a href="#about" data-nav-section="about"></a></li>*/}
-                  <li><a href="#timeline" data-nav-section="timeline">Parcours</a></li>
+                  <li>
+                    <a href="#timeline" data-nav-section="timeline">
+                      Parcours
+                    </a>
+                  </li>
                   {/*<li><a href="#projects" data-nav-section="projects">Réalisations</a></li> ATTENDRE PROJETS SEXY*/}
                 </ul>
               </div>
             </nav>
             <nav id="colorlib-main-menu">
               <ul>
-                <li><a href="https://www.linkedin.com/in/jefferson-afanou/" target="_blank" rel="noopener noreferrer"><i className="icon-linkedin2" /></a></li>
-                <li><a href="https://github.com/afanou-jefferson" target="_blank" rel="noopener noreferrer"><i className="icon-github"></i></a></li>
+                <li>
+                  <a
+                    href="https://www.linkedin.com/in/jefferson-afanou/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i className="icon-linkedin2" />
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://github.com/afanou-jefferson"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i className="icon-github"></i>
+                  </a>
+                </li>
               </ul>
             </nav>
-            <div className="colorlib-footer" style={{position: "absolute", bottom:0}}>
-              <p><small>
-                  <FaCode/> avec React<br></br> 
-                  à partir d'un template(<a href="https://colorlib.com" target="_blank" rel="noopener noreferrer">Colorlib</a>)
-              </small></p>
-              <p><small>
-                
-              </small></p>
+            <div
+              className="colorlib-footer"
+              style={{ position: "absolute", bottom: 0 }}
+            >
+              <p>
+                <small>
+                  <FaCode /> avec React<br></br>à partir d'un template (
+                  <a
+                    href="https://colorlib.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Colorlib
+                  </a>
+                  )
+                </small>
+              </p>
+              <p>
+                <small></small>
+              </p>
             </div>
           </aside>
         </div>
       </div>
-    )
+    );
   }
 }
