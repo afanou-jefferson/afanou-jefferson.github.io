221fbf7850eec9d06d230d2764e0bf6c6f5fa04a
Jefferon AFANOU <jefferson.afanou@gmail.com>
2021-09-22 03:15:15 +0200

    Updates

---
+++ precache-manifest.6de9c5e6a3bb5e6acb1a4bff4a620122.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a95acbe9f3e7b94e552bd183ae0eb04d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44cc1800229cce42d568",
+    "url": "/static/css/main.2430bf53.chunk.css"
+  },
+  {
+    "revision": "3d0c8fa677ebdb3536e4",
+    "url": "/static/js/2.cf8e8390.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.cf8e8390.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44cc1800229cce42d568",
+    "url": "/static/js/main.eaa969b0.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
